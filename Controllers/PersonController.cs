2f003f8beb3c75b158d5d724da905b64d0ae081c
larskrj <larsj@mesan.no>
2012-03-11 16:37:16 +0100

    Lagt til alle filene

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Api.Models;
+
+namespace Api.Controllers
+{
+    public class PersonController : ApiController
+    {
+    	private static int lastId = 0;
+		private static List<Person> persons = new List<Person>();
+
+    	static PersonController()
+    	{
+			AddPerson(new Person { Name = "Lars K.", Age = 33 });
+			AddPerson(new Person { Name = "Stine.", Age = 31 });
+    	}
+		
+		// GET /api/person
+        public IEnumerable<Person> Get()
+        {
+        	return persons;
+        }
+
+        // GET /api/person/5
+        public Person Get(int id)
+        {
+        	var person = persons.FirstOrDefault(o => o.Id == id);
+			if (person == null)
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+
+        	return person;
+        }
+
+        // POST /api/person
+        public HttpResponseMessage<Person> Post(Person person)
+        {
+        	AddPerson(person);
+        	var response = new HttpResponseMessage<Person>(person, HttpStatusCode.Created);
+        	response.Headers.Location = new Uri(Request.RequestUri + "/" + person.Id);
+
+        	return response;
+        }
+
+		// PUT /api/person
+		public void Put(Person person)
+		{
+			int index = persons.FindIndex(p => p.Id == person.Id);
+            if (index == -1)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            persons.RemoveAt(index);
+            persons.Add(person);
+		}
+		
+    	// DELETE /api/person/5
+		public void Delete(int id)
+        {
+			int index = persons.FindIndex(p => p.Id == id);
+			if (index == -1)
+			{
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			persons.RemoveAt(index);
+        }
+
+    	private static Person FindPerson(int id)
+    	{
+    		var person = persons.FirstOrDefault(o => o.Id == id);
+    		if (person == null)
+    			throw new HttpResponseException(HttpStatusCode.NotFound);
+    		return person;
+    	}
+
+    	private static void AddPerson(Person person)
+    	{
+    		person.Id = ++lastId;
+    		persons.Add(person);
+    	}
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Api
+{
+	// Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+	// visit http://go.microsoft.com/?LinkId=9394801
+
+	public class WebApiApplication : System.Web.HttpApplication
+	{
+		public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+		{
+			filters.Add(new HandleErrorAttribute());
+		}
+
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+			routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+			);
+		}
+
+		public static void RegisterHttpRoutes(HttpRouteCollection routes)
+		{
+			routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+		}
+
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+
+			RegisterGlobalFilters(GlobalFilters.Filters);
+			RegisterRoutes(RouteTable.Routes);
+			
+			var config = GlobalConfiguration.Configuration;
+			
+			// Replace the default JsonFormatter with our custom one
+			ConfigureApi(config);
+
+			BundleTable.Bundles.RegisterTemplateBundles();
+		}
+
+		public static void ConfigureApi(HttpConfiguration config)
+		{
+			var index = config.Formatters.IndexOf(config.Formatters.JsonFormatter);
+			config.Formatters[index] = new JsonCamelCaseFormatter();
+
+			config.MessageHandlers.Add(new CorsHandler());
+		}
+	}
+}
